934252f9eb1f3355ed289edb0df42bba88ecb2b6
Rocio-hub <roci0055@easv365.dk>
2021-05-27 11:06:45 +0200

    signup validations

--- app/src/main/java/com/easv/takemehomeapp/Activities/SignUpNormalUserActivity.kt
+++ app/src/main/java/com/easv/takemehomeapp/Activities/SignUpNormalUserActivity.kt
@@ -13,6 +13,7 @@ import kotlinx.android.synthetic.main.activity_create_lostuser_account.button_su
 import kotlinx.android.synthetic.main.activity_sign_up_normal_user.*
 import kotlinx.android.synthetic.main.activity_sign_up_normal_user.editText_cpr
 import java.io.File
+import java.lang.Double.parseDouble
 
 class SignUpNormalUserActivity : AppCompatActivity() {
 
@@ -40,27 +41,57 @@ class SignUpNormalUserActivity : AppCompatActivity() {
             newUser.firstName = editText_firstName.text.toString()
             newUser.lastName = editText_lastName.text.toString()
             newUser.CPR = editText_cpr.text.toString().toInt()
+            normalUserDB.createPrivilegedUser(newUser)
+            val intent = Intent(this, CodeScannerActivity::class.java)
+            intent.putExtra("loggedUser", newUser)
+            startActivity(intent)
         }
-
-        normalUserDB.createPrivilegedUser(newUser)
-        val intent = Intent(this, CodeScannerActivity::class.java)
-        intent.putExtra("loggedUser", newUser)
-        startActivity(intent)
-
         /*
          * TODO Check if user created exists on database already
          */
     }
 
     private fun validateInput(): Boolean {
-        showMissingInfo()
-        return true
-    }
 
-    private fun showMissingInfo() {
-        /* if (editText_fullName.text.isNullOrBlank()) {
-             editText_fullName.error = "Please enter a name"
-         } else editText_fullName.error = null */
+        if (editText_firstName.text.toString() == "" || checkContainNumber(editText_firstName.text.toString()) || editText_firstName.text.toString()
+                .contains(" ")
+        ) {
+            editText_firstName.error =
+                "Please, enter a value for the first name. It cannot contain numbers or spaces."
+            return false
+        }
+        if (editText_lastName.text.toString() == "" || checkContainNumber(editText_lastName.text.toString()) || editText_lastName.text.toString()
+                .contains(" ")
+        ) {
+            editText_lastName.error =
+                "Please, enter a value for the last name. It cannot contain numbers or spaces."
+            return false
+        }
+        try {
+            parseDouble(editText_cpr.text.toString())
+        } catch (e: NumberFormatException) {
+            editText_cpr.error =
+                "Please, enter enter a value for the CPR. It must be a number and not contain spaces."
+        }
+        if (editText_cpr.text.toString() == "" || editText_cpr.text.toString().contains(" ")) {
+            editText_cpr.error =
+                "Please, enter enter a value for the CPR. It must be a number and not contain spaces."
+            return false
+        }
+        if (editText_username.text.toString() == "") {
+            editText_username.error = "Please, enter enter a value for the username"
+            return false
+        }
+        if (editText_password.text.toString() == "") {
+            editText_password.error = "Please, enter enter a value for the password"
+            return false
+        }
+        if (editText_repPassword.text.toString() != editText_password.text.toString()) {
+            editText_repPassword.error = "Passwords do not match"
+            return false
+        }
+        return true
+
     }
 
     private fun onClickPicture() {
@@ -86,7 +117,7 @@ class SignUpNormalUserActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putString("firstName", editText_firstName.text.toString())
         outState.putString("lastName", editText_lastName.text.toString())
-        outState.putInt("cpr", editText_cpr.text.toString().toInt())
+        outState.putString("cpr", editText_cpr.text.toString())
         outState.putString("username", editText_username.text.toString())
         outState.putString("password", editText_password.text.toString())
         outState.putString("repPassword", editText_repPassword.text.toString())
@@ -98,10 +129,20 @@ class SignUpNormalUserActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         editText_firstName.setText(savedInstanceState.getString("firstName"))
         editText_lastName.setText(savedInstanceState.getString("lastName"))
-        editText_cpr.setText(savedInstanceState.getInt("cpr"))
+        editText_cpr.setText(savedInstanceState.getString("cpr"))
         editText_username.setText(savedInstanceState.getString("username"))
         editText_password.setText(savedInstanceState.getString("password"))
         editText_repPassword.setText(savedInstanceState.getString("repPassword"))
         newUser = savedInstanceState.getSerializable("newUser") as BEPrivilegedUser
+    }
+
+    private fun checkContainNumber(value: String): Boolean {
+        var containNumber: Boolean = false
+        for (item in value.toCharArray()) {
+            if (Character.isDigit(item).also { containNumber = it }) {
+                break
+            }
+        }
+        return containNumber
     }
 }
